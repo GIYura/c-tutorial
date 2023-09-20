565c0bb69af82cdfa7cb69736d6bdc5992b02777
JHlotov <iglotov@tektelic.com>
2023-09-20 16:36:53 +0300

    ADD: cases

--- playground/src-0/test.c
+++ playground/src-0/test.c
@@ -3,12 +3,20 @@
 #define CASE_1  0
 #define CASE_2  0
 #define CASE_3  0
-#define CASE_4  1
+#define CASE_4  0
 #define CASE_5  0
 #define CASE_6  0
+#define CASE_7  0
+#define CASE_8  0
+#define CASE_9  0
+#define CASE_10 1
 
 #if CASE_3
 void fun(int **p);
+#endif
+
+#if CASE_7
+void foo(int p[3][4]);
 #endif
 
 /*
@@ -46,7 +54,6 @@ int main(int argc, char* argv[])
     printf("\n\n");
 
 #elif CASE_2
-
 /*
 2. What will be the output of the program?
 */
@@ -134,8 +141,7 @@ The name of 2-D array is the address of the first 1-D array (the whole row).
         }
     }
 #elif CASE_3
-/*
-4. 
+/* 
 Q: What will be the output of the program?
 */
     printf("========= CASE 3 ==========\n");
@@ -152,17 +158,26 @@ NOTE: print 2-D array within single for loop.
         printf("%d ", *p);
     }
     printf("\n");
-#elif CASE_4
 
+#elif CASE_4
 /*
-5.
-Q: What will be the output of the program? 
+Q: What will be the output of the program?
+NOTE:
+distance between pointers = (pointer - pointer) / sizeof(element pointer points to)
+int* p = 0x100;
+int* q = 0x104;
+q - p = (0x104 - 0x100)/4 = 1.
 */
     printf("========= CASE 4 ==========\n");
     int arr[] = { 0, 1, 2, 3, 4 };
 
     int* p[] = { arr, arr + 1, arr + 2, arr + 3, arr + 4 };
     int** ptr = p;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("address of p[%d]: %p\n", i, p[i]);
+    }
 
     ptr++;
 
@@ -179,6 +194,76 @@ Q: What will be the output of the program?
 
     ++*ptr; /* dereference ptr, then increment the value */
     printf("%ld, %ld, %d\n", ptr-p, *ptr-arr, **ptr);
+
+#elif CASE_5
+/*
+Q: What will be the output of the program?
+*/
+    printf("========= CASE 5 ==========\n");
+
+    int a[3][4] = {{1, 2, 3, 4}, {4, 3, 2, 1}, {7, 8, 9, 0}};
+    printf("sizeof a: %ld\n", sizeof(a));
+/*
+NOTE: 
+a + 1 = next row
+&a + 1 = next after the whole matrix
+*/
+    printf("%p\n%p\n", a + 1, &a + 1);
+
+#elif CASE_6
+/*
+Q: What will be the output of the program?
+*/
+    printf("========= CASE 6 ==========\n");
+    int arr[1] = {10};
+    printf("%d\n", 0[arr]);
+    printf("sizeof arr: %ld\n", sizeof(arr));
+
+#elif CASE_7
+/*
+Q. Which of the following is correct way to define the function fun() in the below program?
+*/
+    printf("========= CASE 7 ==========\n");
+    int a[3][4];
+    foo(a);
+
+#elif CASE_8
+/*
+Q. What will be the output of the program ?
+*/
+    printf("========= CASE 8 ==========\n");
+    char s[25] = "The cocaine man";
+    int i = 0;
+    char ch;
+    ch = *(s + ++i);    /* s[++i]; */
+    printf("%c", ch);   /* 'h' */
+    ch = *(s + i++);    /* s[i++]; */
+    printf("%c", ch);   /* 'h' */
+    ch = *(i++ + s);    /* i++[s]; */
+    printf("%c", ch);   /* 'e' */
+    ch = ++*(i + s);    /* ++i[s]; */
+    printf("%c\n", ch); /* '!' */
+
+#elif CASE_9
+/*
+Q. What will be the output of the program?
+*/
+    printf("========= CASE 9 ==========\n");
+    char ch = 'A';
+    printf("%ld, %ld, %ld\n", sizeof(ch), sizeof('a'), sizeof(3.14f));
+
+#elif CASE_10
+/*
+Q. What will be the output of the program?
+A: Error.
+NOTE: str = "ababa"; // illigal
+*/
+    printf("========= CASE 10 ==========\n");
+    char str[] = "Nagpur";
+    str[0]='K';
+    printf("%s, ", str);
+    //str = "Kanpur";
+    printf("%s\n", str + 1);
 #endif
 
     return 0;
@@ -192,5 +277,11 @@ void fun(int **p)
 }
 #endif
 
+/**/
+#if CASE_7
+void foo(int p[3][4])
+{
 
+}
+#endif
 
