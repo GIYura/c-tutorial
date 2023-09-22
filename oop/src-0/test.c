125385d342ee0406c9d3cf684802fa320ed633c1
JHlotov <iglotov@tektelic.com>
2023-09-22 10:43:11 +0300

    UPDATE: makefile and test

--- oop/src-0/test.c
+++ oop/src-0/test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+#define MAX(a, b)   ((a) > (b) ? (a) : (b))
+
 int main (int argc, char* argv[])
 {
+    int a = 10;
+    int b = 5;
+
+    int c = MAX(a, b);
     
+    printf("%d\n", c);
+
     return 0;
 }
