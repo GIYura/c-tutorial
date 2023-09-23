52aee485e474ff15ed78562bac8c63f84428b7c3
JHlotov <iglotov@tektelic.com>
2023-09-23 23:46:14 +0300

    ADD: chapter 2

---
+++ extreme-c/chapter-2/src-0/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "print.h"
+
+void PrintMessage(const char* const message)
+{
+    printf("%s\n", message);
+}
---
+++ extreme-c/chapter-2/src-0/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "print.h"
+
+int main(int argc, char* argv[])
+{
+    PrintMessage("Hello World!");
+
+    return 0;
+}
