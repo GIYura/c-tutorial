d89dbc50b53723bc01c635776058f2449a3661e0
JHlotov <iglotov@tektelic.com>
2023-09-25 13:32:14 +0300

    ADD: chapter 2

---
+++ extreme-c/chapter-2/src-1/func.h
@@ -0,0 +1,7 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int add(int a, int b);
+int mul(int a, int b);
+
+#endif /* FUNC_H  */
---
+++ extreme-c/chapter-2/src-1/mul.c
@@ -0,0 +1,6 @@
+//#include "func.h"
+
+int mul(int a, int b)
+{
+    return a * b;
+}
